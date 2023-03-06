cc7b4a2bb880a45d54286d9ee86b6a6701c1fccb
ElDav1d <dvivoes@yahoo.es>
2023-03-06 14:08:27 +0100

    fetch and display existing data

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,11 +1,43 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import AddForm from "../components/AddForm";
 
 function TodoList() {
+  const [allTodos, setAllTodos] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+
+  //! react STILL NOT ALLOWING async code on CONSTRUCTOR
+  //* go USEEFFECT
+
+  useEffect(() => {
+    getData();
+  }, []); // ComponentDidMount
+
+  const getData = async () => {
+    try {
+      const response = await axios.get("http://localhost:5005/api/todo");
+
+      setAllTodos(response.data);
+      setIsFetching(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (isFetching === true) {
+    return <h3>...loading data</h3>;
+  }
+
   return (
     <div>
       <AddForm />
       <hr />
       <h3>Lista de To-Do</h3>
+      <ul>
+        {allTodos.map(({ title, _id }) => (
+          <li key={_id}>{title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
